1597ab638fccf0b5c04c0af90bed9aaf1408bb78
callystaarviana-coder <callystaarviana@gmail.com>
2026-09-21 01:51:10 +0700

    Menambahkan informasi Education

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,11 @@ from main.views import (
     create_project,
     get_projects_json,
     delete_project,
+    show_education,
+    create_education,
+    update_education,
+    delete_education,
+    get_education_json,
 )
 
 app_name = "main"
@@ -16,8 +21,27 @@ urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
     path("skills/", show_skills, name="show_skills"),
-    path("projects/add/", create_project, name="create_project"),
+
     path("projects/", show_projects, name="show_projects"),
+    path("projects/add/", create_project, name="create_project"),
+    path(
+        "projects/<uuid:project_id>/delete/",
+        delete_project,
+        name="delete_project",
+    ),
     path("api/projects/", get_projects_json, name="get_projects_json"),
-    path("projects/<uuid:project_id>/delete/",delete_project,name="delete_project")
+
+    path("education/", show_education, name="show_education"),
+    path("education/add/", create_education, name="create_education"),
+    path(
+        "education/<int:education_id>/edit/",
+        update_education,
+        name="update_education",
+    ),
+    path(
+        "education/<int:education_id>/delete/",
+        delete_education,
+        name="delete_education",
+    ),
+    path("api/education/", get_education_json, name="get_education_json"),
 ]
